23a4d7b8aacd2c090f06784f2cb7742645a0098b
psychoandtech <118345785+psychoandtech@users.noreply.github.com>
2023-03-15 22:12:52 +0800

    Update Bagging1.py

--- Bagging1.py
+++ Bagging1.py
@@ -21,7 +21,9 @@ file_score2 = open("Basic Rule/result/35 and 65 of rank.txt")
 
 file_score_write = open("bagging_(Lasso Rank)"+str(Weights[0])+"_Basic Rule"+str(Weights[1])+".txt", "w")
 '''
-open函数的作用：
+open函数的作用：参数：一个是路径
+'w'即使文件存在，也会重新创建一个进行 写 的操作
+open函数返回list类型
 '''
 def Read_Score(file_score):
     first_line = 0
@@ -35,7 +37,12 @@ def Read_Score(file_score):
     file_score.close()
     return(score)
 '''
+Read_Score对文件进行输入处理
 file_score的类型：txt
+strip（）：参数为空时默认删除字符串中的空格
+split（','）:以传入的参数为分界，对字符串分割成一个个元素，放进列表中（['aaa','bbb','ccc'])
+ score[line_cur[0]] = float(line_cur[1])：
+ 键名是line_cur[0] 将键值设置为line_cur[1]
 '''
 
 def Merge(s1, s2, x1, x2):
@@ -48,7 +55,10 @@ def Merge(s1, s2, x1, x2):
     file_score1.close()
     file_score2.close()
     return(new_score)
-
+'''
+综合两次成绩进行加权
+返回一个字典，键名是学生名，键值是成绩
+'''
 
 def Test(real_rank, predict_rank):
     error = 0.0
@@ -75,7 +85,7 @@ if __name__ == "__main__":
     print(score2)
     print(score)
     if WRITE == 1:
-        I = []
+        I = []#二维列表
         for i in score:
             I.append((i, score[i]))
         I = sorted(I, key = itemgetter(1))
@@ -89,5 +99,9 @@ if __name__ == "__main__":
             file_score_write.write(str(stu)+", "+str(rank[stu])+'\n')
         print(rank)
     file_score_write.close()
-        
-        
+ '''
+ 列表的append函数：这里添加的是元组：(i, score[i])
+ 列表的sorted函数：第一个参数是I，即排序的对象；key = itemgetter(1)；默认是升序reverse=False
+ itemgetter函数：返回下标为1的元素
+ 在这里是对score[i]为对象进行排序
+ '''
